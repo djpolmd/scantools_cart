e96d1cca0b997c7e673ac1747e06690ce1005bb9
root <djpolmd@gmail.com>
2017-10-12 10:20:47 +0300

    footer changed+img

--- catalog/language/ru-ru/common/footer.php
+++ catalog/language/ru-ru/common/footer.php
@@ -14,5 +14,5 @@ $_['text_account']      = 'Личный Кабинет';
 $_['text_order']        = 'История заказов';
 $_['text_wishlist']     = 'Закладки';
 $_['text_newsletter']   = 'Рассылка';
-$_['text_powered']      = 'Работает на <a href="http://opencart-russia.ru">OpenCart "Русская сборка"</a><br /> %s &copy; %s';
+$_['text_powered']      = 'Работает на <a href="https://github.com/djpolmd/scantools_cart.git"> ScanTools "RUS/RO"</a><br /> %s &copy; %s';
 
